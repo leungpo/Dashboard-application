c70eb7b81dd0ecc57d44b60723b6e3f44340a8af
leungpo <pollywsleung@gmail.com>
2023-03-19 13:12:19 +0800

    feat: add NavBar left side

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path = "/" element={<Navigate to="/dashboard" replace />}/>
-              <Route path = "/dashboard" element = {<Dashboard />} />        
+              <Route path = "/dashboard" element = {<Dashboard />} />       
             </Route>
           </Routes> 
         </ThemeProvider>
